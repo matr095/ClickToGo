5883beebb7678bc34feb02afd0c652e8adf417f8
MatR <matr@mac-mini-de-matr.home>
2015-11-14 12:24:06 +0100

    First add

--- LinkGoto/ViewController.swift
+++ LinkGoto/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tf: UITextField!
+    
+    @IBOutlet weak var Tricher: UIButton!
+    @IBOutlet weak var nombre: UILabel!
+    
+    @IBOutlet weak var go: UILabel!
+    
+    @IBOutlet weak var webV: UIWebView!
+    var address: String = ""
+    var counter: Int = 0
+    
 
+    @IBAction func toucher(sender: UIButton) {
+        let adresse:String = tf.text!
+        counter += 1
+        nombre.text = "Triches: " + String(counter)
+        let url = NSURL(string: adresse)!
+        webV.loadRequest(NSURLRequest(URL: url))
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 
 }
+
+
+
 
